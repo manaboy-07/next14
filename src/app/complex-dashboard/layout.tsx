e83d18d31c05c55fd0c8d7f7af4263612c13f250
Manasseh Oruebor <manassehoruebor@gmail.com>
2024-02-09 01:02:46 +0100

    relearning old stuff

--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -3,15 +3,18 @@ import React from "react";
 export default function DashboardLayout({
   children,
   users,
+  login,
   notifications,
   revenue,
 }: {
   children: React.ReactNode;
+  login: React.ReactNode;
   users: React.ReactNode;
   revenue: React.ReactNode;
   notifications: React.ReactNode;
 }) {
-  return (
+  const isLoggedIn = true;
+  return isLoggedIn ? (
     <>
       <div>{children}</div>
       <div style={{ display: "flex" }}>
@@ -22,11 +25,12 @@ export default function DashboardLayout({
         <div style={{ display: "flex", flex: 1 }}>{notifications}</div>
       </div>
     </>
-
+  ) : (
     // useranalytics
     //revenue numetirc
     //notifications
     //These are all components
+    login
   );
 }
 //you have 3 slots , those that have the @ symbol on thier folder name or sturcture
---
+++ src/app/f1/(.)f2/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function InterceptedF2() {
+  return (
+    <>
+      <h1>F2 intercepted page</h1>
+    </>
+  );
+}
